af3770fd7cc0d5c5c6ebadbee47872edbab88649
Jon Pinto <JonPinto@protonmail.com>
2022-12-06 22:35:28 -0500

    finished code

--- main.js
+++ main.js
@@ -45,7 +45,7 @@ function saveIssue(e) {
         assignedTo: issueAssignedTo,
         status: issueStatus
     }
-    
+
     if(localStorage.getItem('issues')===null) {
         let issues = []
         issues.push(issue)
@@ -74,4 +74,18 @@ function setStatusClosed(id){
   localStorage.setItem('issues', JSON.stringify(issues))
 
   fetchIssues()
+}
+
+function deleteIssue (id) {
+  let issues = JSON.parse(localStorage.getItem('issues'))
+  for(let i=0; i < issues.length; i++){
+    if(issues[i].id === id){
+      issues.splice(i,1)
+    }
+  }
+
+  localStorage.setItem('issues', JSON.stringify(issues))
+
+  fetchIssues()
+
 }
