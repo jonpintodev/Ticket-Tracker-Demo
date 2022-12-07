7500982465d265276b40302dbfba26797e23e011
Jon Pinto <JonPinto@protonmail.com>
2022-12-06 21:17:51 -0500

    building functions

---
+++ main.js
@@ -0,0 +1,38 @@
+document.getElementById('issueInputForm').addEventListener('submit', saveIssue)
+
+function fetchIssues(){
+  let issues = JSON.parse(localStorage.getItem('issues'))
+  let issuesList = document.getElementById('issuesList')
+
+  issuesList.innerHTML = '';
+
+  for (let i = 0; i < issues.length; i++) {
+    let id = issues[i].id
+    let subject = issues[i].id
+    let description = issues[i].id
+    let severity = issues[i].id
+    let assignedTo = issues[i].id
+    let status = issues[i].status
+    let statusColor = issues == "Closed" ? 'label-success' : 'label-info'
+
+    //issuesList.innerHTML +=
+  }
+}
+
+function saveIssue(e) {
+  let issueId = chance.guid()
+  let issueSubject = document.getElementById('issueSubjectInput').value
+  let issueDesc = document.getElementById('issueDescInput').value
+  let issueSeverity = document.getElementById('issueSeverityInput').value
+  let issueAssignedTo = document.getElementById('issueAssignedToInput').value
+  let issueStatus = 'Open'
+
+  let issue = {
+    id: issueId,
+    subject: issueSubject,
+    description: issueDesc,
+    severity: issueSeverity,
+    assignedTo: issuesAssignedTo,
+    status: issueStatus 
+  }
+}
