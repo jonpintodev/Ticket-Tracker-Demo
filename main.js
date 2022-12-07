ed3163747b829945abd1fb52bf14dd5602726eca
Jon Pinto <JonPinto@protonmail.com>
2022-12-06 21:59:08 -0500

    save and fetch functions working

--- main.js
+++ main.js
@@ -1,54 +1,58 @@
-document.getElementById('issueInputForm').addEventListener('submit', saveIssue)
+document.getElementById('issueInputForm').addEventListener('submit',saveIssue)
 
-function fetchIssues(){
-  let issues = JSON.parse(localStorage.getItem('issues'))
-  let issuesList = document.getElementById('issuesList')
 
-  issuesList.innerHTML = '';
+function fetchIssues() {
+    let issues = JSON.parse(localStorage.getItem('issues'))
+    let issuesList = document.getElementById('issuesList')
+    console.log(issues)
 
-  for (let i = 0; i < issues.length; i++) {
-    let id = issues[i].id
-    let subject = issues[i].id
-    let description = issues[i].id
-    let severity = issues[i].id
-    let assignedTo = issues[i].id
-    let status = issues[i].status
-    let statusColor = issues == "Closed" ? 'label-success' : 'label-info'
+    issuesList.innerHTML = '';
 
-    //issuesList.innerHTML +=
-  }
+    for (let i = 0; i < issues.length; i++) {
+        let id = issues[i].id
+        let subject = issues[i].subject
+        let description = issues[i].description
+        let severity = issues[i].severity
+        let assignedTo = issues[i].assignedTo
+        let status = issues[i].status
+        let statusColor = status == "Closed" ? 'label-success' : 'label-info'
+
+        //issuesList.innerHTML +=
+        issuesList.innerHTML += 
+        '<div class="well">' +
+        '<h6>Issue ID:' + id + '</h6>' +
+        '<p><span class= "label ' + statusColor + ' ">' + status + '</span></p>' +
+        '<h3>' + subject + '</h3>' +
+        '<p>' + description + '</p>'
+        + '</div>'
+    }
 }
 
 function saveIssue(e) {
-  let issueId = chance.guid()
-  let issueSubject = document.getElementById('issueSubjectInput').value
-  let issueDesc = document.getElementById('issueDescInput').value
-  let issueSeverity = document.getElementById('issueSeverityInput').value
-  let issueAssignedTo = document.getElementById('issueAssignedToInput').value
-  let issueStatus = 'Open'
-
-  let issue = {
-    id: issueId,
-    subject: issueSubject,
-    description: issueDesc,
-    severity: issueSeverity,
-    assignedTo: issuesAssignedTo,
-    status: issueStatus 
-  }
-
-  if(localStorage.getItem('issues')===null){
-    let issues = []
-    issues.push(issue)
-    localStorage.setItem('issues', JSON.stringify(issues))
-  } else {
-    let issues = JSON.parse(localStorage.getItem('issues'))
-    issues.push(issue)
-    localStorage.setItem('issues', JSON.stringify(issues))
-  }
-
-  document.getElementById('issueInputForm').reset();
-
-  fetchIssues()
-
-  e.preventDefault()
+    let issueId = chance.guid()
+    let issueSubject = document.getElementById('issueSubjInput').value
+    let issueDesc = document.getElementById('issueDescInput').value
+    let issueSeverity = document.getElementById('issueSeverityInput').value
+    let issueAssignedTo = document.getElementById('issueAssignedToInput').value
+    let issueStatus = 'Open'
+    let issue = {
+        id: issueId,
+        subject: issueSubject,
+        description: issueDesc,
+        severity: issueSeverity,
+        assignedTo: issueAssignedTo,
+        status: issueStatus
+    }
+    if(localStorage.getItem('issues')===null) {
+        let issues = []
+        issues.push(issue)
+        localStorage.setItem('issues', JSON.stringify(issues))
+    } else {
+        let issues = JSON.parse(localStorage.getItem('issues'))
+        issues.push(issue)
+        localStorage.setItem('issues', JSON.stringify(issues))
+    }
+    document.getElementById('issueInputForm').reset();
+    fetchIssues()
+    e.preventDefault()
 }
